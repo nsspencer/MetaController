0e7c314028e953aa20090cae729b8df1412d8e44
Nathan Spencer <nathanspencer97@hotmail.com>
2024-05-10 09:54:37 -0400

    updated setup.py and added __all__ in init

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     description="A Python library for managing controlled logic.",
     long_description_content_type="text/markdown",
     url="https://github.com/nsspencer/ControllerPy",
-    packages=find_packages(),
+    packages=find_packages(include=["pycontroller"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    test_suite="test",
+    zip_safe=True,
 )
